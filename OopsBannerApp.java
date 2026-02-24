625c95964ad2028749a03b5d4b78e4438ffc18db
Karthikeya Kuncham <kunchamsrinivasakarthikeya-code>
2026-02-24 11:39:08 +0530

    first commit

--- OopsBannerApp.java
+++ OopsBannerApp.java
@@ -1,12 +1,11 @@
 public class OopsBannerApp {
-//uc2
     public static void main(String[] args) {
-        System.out.println("   ***   " + " " + "   ***** ");
-        System.out.println(" **   ** " + " " + " **      ");
-        System.out.println("**     **" + " " + "**       ");
-        System.out.println("**     **" + " " + "  *****  ");
-        System.out.println("**     **" + " " + "      ** ");
-        System.out.println(" **   ** " + " " + " **   ** ");
-        System.out.println("   ***   " + " " + "  *****  ");
+    System.out.println(String.join(" ", "   ***   ", "   ***** "));
+    System.out.println(String.join(" ", " **   ** ", " **      "));
+    System.out.println(String.join(" ", "**     **", "**       "));
+    System.out.println(String.join(" ", "**     **", "  *****  "));
+    System.out.println(String.join(" ", "**     **", "      ** "));
+    System.out.println(String.join(" ", " **   ** ", " **   ** "));
+    System.out.println(String.join(" ", "   ***   ", "  *****  ")); 
     }
 }
